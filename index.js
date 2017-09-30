52742afe424d3992528d9cc0fac176faf88fe8b7
Nick Papoutsis <izybit@gmail.com>
2017-09-30 14:22:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,34 @@ const app = "I don't do much."
 function destructivelyAppendKitten(name){
   return kittens.push(name);
 }
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift();
+}
+
+function appendKitten(name){
+  let arr = kittens.push(name);
+  return arr;
+}
+
+function prependKitten(name){
+  let arr = kittens.unshift(name);
+  return arr;
+}
+
+function removeLastKitten(){
+  let arr = kittens.pop;
+  return arr;
+}
+
+function removeFirstKitten() {
+  return kittens.unshift();
+}
